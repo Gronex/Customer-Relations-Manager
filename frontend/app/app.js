d3a944c5900d732148dd60e3eac05d15d1d9d02e
Mads Slotsbo <mads.slotsbo@gmail.com>
2016-02-10 14:21:35 +0100

    setup frontend

---
+++ frontend/app/app.js
@@ -0,0 +1,43 @@
+﻿(function () {
+  'use strict';
+
+  angular.module('app', [
+    // Angular modules
+    'ngAnimate',
+    // Custom modules
+
+    // 3rd Party Modules
+    'ui.router',
+    'ui.materialize'
+  ])
+  .config(function($stateProvider, $urlRouterProvider) {
+    //
+    // For any unmatched url, redirect to /state1
+    $urlRouterProvider.otherwise("/state1");
+    //
+    // Now set up the states
+    $stateProvider
+      .state('state1', {
+        url: "/state1",
+        templateUrl: "view/app/views/state1.html"
+      })
+      .state('state1.list', {
+        url: "/list",
+        templateUrl: "view/app/views/state1.list.html",
+        controller: function($scope) {
+          $scope.items = ["A", "List", "Of", "Items"];
+        }
+      })
+      .state('state2', {
+        url: "/state2",
+        templateUrl: "view/app/views/state2.html"
+      })
+      .state('state2.list', {
+        url: "/list",
+        templateUrl: "view/app/views/state2.list.html",
+        controller: function($scope) {
+          $scope.things = ["A", "Set", "Of", "Things"];
+        }
+      });
+  });
+})();
---
+++ frontend/gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp');
+
+var paths = {
+  js: ["scripts/**/*.js", "app/**/*.js"],
+  html: ["app/**/*.html"],
+  css: ["content/**/*.css"],
+  libs: ["bower_components/**"],
+  index: "index.html"
+};
+
+var dest = "../customer_relations_manager";
+var fullDest = dest + "/view";
+
+gulp.task("move:js", function () {
+  return gulp.src(paths.js, {base: "./"})
+    .pipe(gulp.dest(fullDest));
+});
+
+gulp.task("move:html", function () {
+  return gulp
+    .src(paths.html, {base: "./"})
+    .pipe(gulp.dest(fullDest));
+});
+
+gulp.task("move:index", function () {
+  return gulp
+    .src(paths.index, {base: "./"})
+    .pipe(gulp.dest(dest));
+});
+
+gulp.task("move:css", function () {
+  return gulp
+    .src(paths.css, {base: "./"})
+    .pipe(gulp.dest(fullDest));
+});
+
+gulp.task("move:libs", function () {
+  return gulp
+    .src(paths.libs, {base: "./"})
+    .pipe(gulp.dest(fullDest));
+});
+
+gulp.task("move", ["move:js", "move:html", "move:css", "move:libs", "move:index"]);
+
+gulp.task("default", ["move"]);
