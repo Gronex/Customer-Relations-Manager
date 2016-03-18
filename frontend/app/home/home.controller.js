570c9a053899462b396cf05f4954617a51da927e
Mads Slotsbo <mads.slotsbo@gmail.com>
2016-03-18 11:43:04 +0100

    visual support for advanced filters

--- frontend/app/home/home.controller.js
+++ frontend/app/home/home.controller.js
@@ -7,21 +7,28 @@
     .module('CRM')
     .controller('Home', Home);
 
-  Home.$inject = ['graph', '$state', '$stateParams'];
+  Home.$inject = ['$scope', 'graph', '$state', '$stateParams', 'dataservice'];
 
   /* @ngInject */
-  function Home(graph, $state, $stateParams) {
+  function Home($scope, graph, $state, $stateParams, dataservice) {
     var vm = this;
     vm.filter = {};
+    vm.savedFilter = {name: "select filter"};
+    vm.savedFilters = [];
+    vm.advancedFilter = {};
     var filter = {};
 
     vm.getProductionGraph = getProductionGraph;
+    vm.getFilter = getFilter;
+    vm.save = save;
 
     activate();
 
     function activate() {
       setupFilter();
       getProductionGraph();
+      getFilters();
+      getFilterOptions();
     }
 
     function getProductionGraph(){
@@ -40,6 +47,60 @@
         });
     }
 
+    function getFilters(){
+      return dataservice.productionGraphFilters
+        .get()
+        .then(function(filters){
+          vm.savedFilters = filters;
+          vm.savedFilters.splice(0,0,{name: "select filter"});
+        });
+    }
+
+    function getFilter(){
+      return dataservice.productionGraphFilters
+        .get(vm.savedFilter.id)
+        .then(function(f){
+          vm.advancedFilter = f;
+          vm.advancedFilterChanged = false;
+        });
+    }
+
+    function getFilterOptions(){
+      vm.filterOptions = {};
+      var departmentTask = dataservice.departments
+            .get()
+            .then(function(departments){
+              vm.filterOptions.departments = departments;
+            });
+      var stageTask = dataservice.stages
+            .get()
+            .then(function(stages){
+              vm.filterOptions.stages = stages;
+            });
+      var userGroupTask = dataservice.userGroups
+            .get()
+            .then(function(userGroups){
+              vm.filterOptions.userGroups = userGroups;
+            });
+      var userTask = dataservice.users
+            .get()
+            .then(function(users){
+              vm.filterOptions.users = users.data;
+            });
+      var categoryTask = dataservice.opportunityCategories
+            .get()
+            .then(function(categories){
+              vm.filterOptions.categories = categories;
+            });
+      return [departmentTask, stageTask, userGroupTask, userTask, categoryTask];
+    }
+
+    $scope.$watch('vm.advancedFilter', function(newValue, oldValue){
+      if(newValue.name === oldValue.name){
+        vm.advancedFilterChanged = true;
+      }
+    },true);
+
     function setupFilter() {
       filter = _.merge({
         fromDate: moment.utc().startOf('year').format(dateFormatString),
@@ -50,6 +111,16 @@
         fromDate: moment.utc(filter.fromDate).toDate(),
         toDate: moment.utc(filter.toDate).toDate()
       };
+    }
+
+    function save(state){
+      if(state === "new"){
+        console.log("save...");
+        console.log(vm.advancedFilter);
+      } else {
+        console.log("save");
+        console.log(vm.advancedFilter);
+      }
     }
   }
 })();
